f57f9c795fc66e4775e489c8980c370dfb46e57c
Alex <alexman258@gmail.com>
2019-10-09 17:09:07 -0700

    Wrote out stub functions

--- hw3.py
+++ hw3.py
@@ -2,6 +2,12 @@
 #    'Kelly': {'task1': 8, 'task3': 5},   'Alex': {'task1': 11, 
 #    'task2': 2, 'task3': 1},  'Aaron': {'task2': 15}, 'Ethan':
 #    'task3': 12}, 'Helen': {'task3': 10}}
+Aleksandr Mandzyuk
+CptS 355
+Assignment 3
+
+
+# Dictionaries
 
 def sprintLog(sprnt):
     d = {}
@@ -11,9 +17,32 @@ def sprintLog(sprnt):
             d[t][k] = h
     return d
 
+def addSprints(sprint1, sprint2):
+    return None
+
+def addNLogs(logList):
+    return None
+
+
+# Dictionaries and Lists
 
 def lookupVal(L,k):
     for x in reversed(L):
         if x.get(k) is not None:
             return x.get(k)
+    return None
+
+def lookupVal2(tL,k):
+    return None
+
+def unzip(L):
+    return None
+
+def numPaths(m, n, blocks):
+    return None
+
+class iterFile():
+
+
+def wordHistogram(words):
     return None
